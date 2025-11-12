802963419c9698107d84a3e3caa2fe1f4671995d
Afsath K H <afsa.ali735@gmail.com>
2025-11-12 15:55:18 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,6 @@
+s_area = lambda s:s*s
+r_area = lambda l,w:l*w
+t_area = lambda b,h:0.5*b*h
+print("area of a trinagle:",t_area(5))
+print("area of a square:",s_area(5))
+print("area of a rectangle:",r_area(5))
